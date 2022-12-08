df5a90f11b7ac0febcb10c898a2193f08e8ec9ee
Mauricio Balderrama Ali <74360477+mabatule@users.noreply.github.com>
2022-12-08 16:08:02 -0400

    adding models for use in repository

---
+++ Jobs-BackEnd/BusinessLogicLayer/UserModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Jobs_BackEnd.BusinessLogicLayer
+{
+    public class UserModel
+    {
+        public long? IdUser { get; set; }
+        public String Nombre { get; set; }
+        public String Username { get; set; }
+        public String ApellidoPaterno { get; set; }
+        public String ApellidoMaterno { get; set; }
+        public String Email { get; set; }
+        public String Rol { get; set; }
+        public String Password { get; set; }
+        public UserModel()
+        {
+            Rol = "employee";
+        }
+    }
+}
